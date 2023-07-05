1c4c2064a2542a61876acaf5cf9385d260f1f6bd
Jorge Campero <jorgecamper0@hotmail.com>
2023-07-04 17:17:33 -0700

    Implementacion del lazy loading y loading skeleton

--- src/main.js
+++ src/main.js
@@ -9,7 +9,16 @@ const api = axios.create({
 });
 //Utilis
 
-function createMovies(movies,container) {
+const lazyLoader = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const url = entry.target.getAttribute('data-img')
+        entry.target.setAttribute('src', url);
+      }
+    });
+  });
+
+function createMovies(movies,container, lazyLoad = false) {
     container.innerHTML = "";
 
 
@@ -23,9 +32,13 @@ function createMovies(movies,container) {
         const movieImg = document.createElement("img")
         movieImg.classList.add("movie-img")
         movieImg.setAttribute("alt", movie.title);
-        movieImg.setAttribute("src", "https://image.tmdb.org/t/p/w300/" + movie.poster_path
+        movieImg.setAttribute(
+            lazyLoad ? 'data-img' : 'src',
+             "https://image.tmdb.org/t/p/w300/" + movie.poster_path
         );
-
+        if (lazyLoad) {
+            lazyLoader.observe(movieImg);
+          }
         movieContainer.appendChild(movieImg);
         container.appendChild(movieContainer);
    })
@@ -58,7 +71,7 @@ async function getCategoriesPreview() {
     const {data} = await api("genre/movie/list?&language=es");
     const categories = data.genres;
 
-    createCategories(categories, categoriesPreviewList);
+    createCategories(categories, categoriesPreviewList,);
 };
 
 async function getTrendingMoviesPreview() {
@@ -66,7 +79,7 @@ async function getTrendingMoviesPreview() {
 
     const movies = data.results;
 
-    createMovies(movies, trendingMoviesPreviewList);
+    createMovies(movies, trendingMoviesPreviewList, true);
 };
 
 async function getMoviesByCategory(id) {
